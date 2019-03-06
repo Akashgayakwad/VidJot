d4b7599b53dee09ab0704fb1864913041d2e03f7
Akash Gayakwad <akashgayakwad123@gmail.com>
2019-03-07 01:12:06 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/vidjot-dev',{
+  useMongoClient: true
+})
+.then(()=>console.log("mongodb connected...."))
+.catch(err => console.log(err));
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+app.use(function(req, res, next){
+  req.name = 'Akash Gayakwad';
+  next();
+});
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extende: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  const title = 'welcome!';
+  res.render("index",{title:title});
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get('/ideas/add', (req, res) => {
+  res.render("ideas/add");
+});
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
+  if(errors.length > 0){
+    res.render('ideas/add',{
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  }
+  else{
+      res.send('passed');
+    }
+});
+
+
+const port = 5000;
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
+});
