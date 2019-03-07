878bbc46399a3c0c8dde1dcbcd6e49568481932b
Akash Gayakwad <akashgayakwad123@gmail.com>
2019-03-07 20:53:04 +0530

    Authentication routes added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const session = require('express-session');
 
 const app = express();
 
+const ideas = require('./routes/ideas');
+
+const users = require('./routes/users');
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/vidjot-dev',{
@@ -16,8 +20,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 .then(()=>console.log("mongodb connected...."))
 .catch(err => console.log(err));
 
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
+
 
 app.use(function(req, res, next){
   req.name = 'Akash Gayakwad';
@@ -58,78 +61,9 @@ app.get('/about', (req, res) => {
   res.render("about");
 });
 
-app.get('/ideas', (req, res) =>{
-  Idea.find({})
-  .sort({date:'desc'})
-  .then(ideas =>{
-    res.render('ideas/index',{
-      ideas:ideas
-    });
-  });
-});
+app.use('/ideas',ideas);
 
-app.get('/ideas/add', (req, res) => {
-  res.render("ideas/add");
-});
-
-app.get('/ideas/edit/:id', (req, res) => {
-  Idea.findOne({_id:req.params.id})
-  .then(idea=>{
-    res.render('ideas/edit',{idea:idea});
-  });
-});
-
-app.post('/ideas', (req, res) => {
-  let errors = [];
-  if(!req.body.title){
-    errors.push({text:'Please add a title'});
-  }
-  if(!req.body.details){
-    errors.push({text:'Please add some details'});
-  }
-  if(errors.length > 0){
-    res.render('ideas/add',{
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    })
-  }
-  else{
-      const newUser = {
-        title: req.body.title,
-        details: req.body.details
-      }
-      new Idea(newUser)
-      .save()
-      .then(idea =>{
-      req.flash('success_msg', 'Video idea added');
-      res.redirect('/ideas');
-      })
-    }
-});
-
-app.put('/ideas/:id',(req, res) =>{
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea=>{
-    idea.title = req.body.title,
-    idea.details = req.body.details,
-    idea.save()
-    .then(idea=>{
-      req.flash('success_msg', 'Video idea updated');
-      res.redirect('/ideas');
-    });
-  });
-});
-
-app.delete('/ideas/:id',(req,res)=>{
-  Idea.remove({_id:req.params.id})
-  .then(()=>{
-    req.flash('success_msg', 'Video idea removed');
-    res.redirect('/ideas');
-  });
-});
+app.use('/users',users);
 
 const port = 5000;
 
